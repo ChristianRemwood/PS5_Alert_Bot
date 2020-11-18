06980a8c76e84e1734af9dbfd29edca56d138235
ChristianRemwood <35041814+ChristianRemwood@users.noreply.github.com>
2020-11-18 13:18:58 -0700

    Updated to make further changes easier

--- Alert_Bot.py
+++ Alert_Bot.py
@@ -1,63 +1,44 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+sites = [
+    "https://www.amazon.com/PlayStation-5-Console/dp/B08FC5L3RG?ref_=ast_sto_dp"
+    ,"https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149"
+    ,"https://www.target.com/p/playstation-5-console/-/A-81114595"
+    ,"https://www.walmart.com/ip/PlayStation5-Console/363472942"
+    ,"https://deals.dell.com/en-us/compare/64x0"
+    ,"https://www.newegg.com/p/N82E16868110292"
+    ,"https://www.gamestop.com/video-games/playstation-5/consoles/products/playstation-5/11108140.html"
+]
+lookFor = [
+    ["Currently unavailable.","$499"]
+    ,"Sold Out"
+    ,"Out of stock"
+    ,["Out of stock","online only at"]
+    ,"Sorry, this deal has sold out."
+    ,"OUT OF STOCK."
+    ,"OUT OF STOCK"
+    ]
+status = {}
 #from twilio.rest import Client
 #client = Client("your_twillio_keys", "your_twillio_id")
-driver = webdriver.Chrome("'ChangeToPath'/chromedriver.exe")
-driver.get("https://www.amazon.com/PlayStation-5-Console/dp/B08FC5L3RG?ref_=ast_sto_dp")
-print(driver.title)
-el = driver.find_element_by_tag_name('body')
-str1=el.text 
-if(str1.find("Currently unavailable.")!=-1):
-    print("its still not available my guy")
-else: 
-    if(str1.find("$499")!=-1):
-        print("yeet")
+driver = webdriver.Chrome("C:/Users/cremwood/Downloads/chromedriver_win32/chromedriver.exe")
+for currentSite in range(len(sites)):
+    driver.get(sites[currentSite])
+    print("Checking:" + driver.title)
+    el = driver.find_element_by_tag_name('body')
+    str1=el.text 
+    if (type(lookFor[currentSite]) == list):
+        if(str1.find(lookFor[currentSite][0])!=-1):
+            status[driver.title] = "Not Available"
+        elif (str1.find(lookFor[currentSite][1])!=-1):
+            status[driver.title] = "In Stock"
+        else:
+            status[driver.title] = "In Stock from Scalpers, or restock scheduled"
     else:
-        print("Scalpers selling it")
+        if(str1.find(lookFor[currentSite])!=-1):
+            status[driver.title] = "Not Available"
+        else:
+            status[driver.title] = "In Stock"
 
-driver.get("https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149")
-print(driver.title)
-el = driver.find_element_by_tag_name('body')
-str1=el.text 
-if(str1.find("Sold Out")!=-1):
-    print("its still not available my guy")
-else: 
-    print("yeet")
-
-driver.get("https://www.target.com/p/playstation-5-console/-/A-81114595")
-print(driver.title)
-el = driver.find_element_by_tag_name('body')
-str1=el.text 
-if(str1.find("Out of stock")!=-1):
-    print("its still not available my guy")
-else: 
-    print("yeet")
-
-driver.get("https://www.walmart.com/ip/PlayStation5-Console/363472942")
-print(driver.title)
-el = driver.find_element_by_tag_name('body')
-str1=el.text 
-if(str1.find("Out of stock")!=-1):
-    print("its still not available my guy")
-else: 
-    print("yeet")
-
-driver.get("https://deals.dell.com/en-us/compare/64x0")
-print(driver.title)
-el = driver.find_element_by_tag_name('body')
-str1=el.text 
-if(str1.find("Sorry, this deal has sold out.")!=-1):
-    print("its still not available my guy")
-else: 
-    print("yeet")
-
-driver.get("https://www.newegg.com/p/N82E16868110292")
-print(driver.title)
-el = driver.find_element_by_tag_name('body')
-str1=el.text 
-if(str1.find("OUT OF STOCK.")!=-1):
-    print("its still not available my guy")
-else: 
-    print("yeet")
-  
+print (status)
 driver.close()
